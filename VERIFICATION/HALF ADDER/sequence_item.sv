58fdc7fcc6c21504b433eb65e10bb6f20f7c8f5f
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-06-25 19:26:14 +0530

    Create sequence_item.sv

---
+++ VERIFICATION/HALF ADDER/sequence_item.sv
@@ -0,0 +1,16 @@
+class ha_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(ha_seq_item);
+  
+  rand bit a;
+  rand bit b;
+  bit sum;
+  bit carry;
+  
+  function new(string name = "ha_seq_item");
+    super.new(name);
+  endfunction
+  
+endclass
+  
+  
