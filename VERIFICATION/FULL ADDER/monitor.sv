c40234be5264648a6b0e2604c821228b9e3e68b4
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-06-28 20:31:48 +0530

    Create monitor.sv

---
+++ VERIFICATION/FULL ADDER/monitor.sv
@@ -0,0 +1,42 @@
+class fa_monitor extends uvm_monitor;
+
+  `uvm_component_utils(fa_monitor)
+
+  fa_seq_item tx;
+  virtual intf intff; 
+  uvm_analysis_port#(fa_seq_item) item_collected_port;
+
+  function new(string name = "fa_monitor", uvm_component parent);
+    super.new(name, parent);
+    item_collected_port = new("item_collected_port", this);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    if (!uvm_config_db#(virtual intf)::get(this, "*", "intff", intff)) begin
+      `uvm_fatal("NOVIF", "Virtual interface get failed")
+    end
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    repeat (8) begin
+      #1;
+      
+      tx = fa_seq_item::type_id::create("tx");
+      tx.a     = intff.a;
+      tx.b     = intff.b;
+      tx.c     = intff.c;
+      tx.sum   = intff.sum;
+      tx.carry = intff.carry;
+
+      `uvm_info("MONITOR TASK",
+        $sformatf("TIME=%0t,a=%b,b=%b,c=%b,sum=%b,carry=%b",
+        $time, tx.a, tx.b, tx.c, tx.sum, tx.carry),
+        UVM_LOW)
+
+      item_collected_port.write(tx);
+      #2;
+    end
+  endtask
+endclass
