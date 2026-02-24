8d7e0ca61b8c6b90e25f3e9b65eef4210c49fed8
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2026-02-24 18:00:14 +0530

    Add UVM environment for APB protocol verification

---
+++ VERIFICATION/APB PROTOCOL WITH RANDOMIZED WR, RD, COV/TESTBENCH/uvm_environment.sv
@@ -0,0 +1,44 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class apb_env extends uvm_env;
+
+  `uvm_component_utils(apb_env)
+
+  // ----------------------------------
+  // Components
+  // ----------------------------------
+  apb_agent      agent;
+  apb_scoreboard scoreboard;
+  apb_coverage  cov;
+
+  // ----------------------------------
+  // Constructor
+  // ----------------------------------
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  // ----------------------------------
+  // Build phase
+  // ----------------------------------
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    agent = apb_agent::type_id::create("agent", this);
+    scoreboard = apb_scoreboard::type_id::create("scoreboard", this);
+    cov = apb_coverage::type_id::create("cov", this);
+  endfunction
+
+  // ----------------------------------
+  // Connect phase
+  // ----------------------------------
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+
+    // Connect monitor to scoreboard
+    agent.monitor.ap.connect(scoreboard.item_collected_imp);
+agent.monitor.ap.connect(cov.analysis_export);  
+  endfunction
+
+endclass
